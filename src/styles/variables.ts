5d09c5bb9a838d9bd8b05409f3fb3c7feaa71e5e
Mohammad Sharifi <mohammadsharifi9978@gmail.com>
2022-09-04 16:47:17 +0300

    feat: add style variables

---
+++ src/styles/variables.ts
@@ -0,0 +1,15 @@
+const spacings = {
+  sm: "1rem",
+  md: "2rem",
+  lg: "3rem",
+} as const;
+
+const fontSizes = {
+  sm: "0.5 rem",
+  md: "1rem",
+  lg: "1.5rem",
+} as const;
+
+const headerHeight = "45px";
+
+export { spacings, fontSizes, headerHeight };
