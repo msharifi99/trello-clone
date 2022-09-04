8a972f6cb2ee82e101fe857d4b9524b038fe1c91
Mohammad Sharifi <mohammadsharifi9978@gmail.com>
2022-09-04 17:05:52 +0300

    feat: add Board componet

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,11 @@
+import Board from "components/Board";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div>
       <Header />
+      <Board />
     </div>
   );
 }
---
+++ src/components/Board/index.tsx
@@ -0,0 +1,11 @@
+function Board(): JSX.Element {
+  return (
+    <article>
+      <header>
+        <h2>Test Board</h2>
+      </header>
+    </article>
+  );
+}
+
+export default Board;
