271fecfa81fe32e5010eb0338fce8f9ed0513cc8
Mohammad Sharifi <mohammadsharifi9978@gmail.com>
2022-09-04 23:10:50 +0300

    feat: use useCard in Card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,11 +1,21 @@
 import { Card as CardType } from "@types";
+import useCard from "hooks/useCard";
 
 type CardProps = {
   id: CardType["id"];
 };
 
 function Card({ id }: CardProps): JSX.Element {
-  return <section>Card {id}</section>;
+  const { card } = useCard(id);
+
+  return (
+    <section>
+      <header>
+        <h2>{card.title}</h2>
+      </header>
+      <p>{card.description}</p>
+    </section>
+  );
 }
 
 export default Card;
