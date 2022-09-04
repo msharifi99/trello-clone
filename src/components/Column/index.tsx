86d53e59da47ed3e4721b7c9d4761e47a96fd722
Mohammad Sharifi <mohammadsharifi9978@gmail.com>
2022-09-04 17:29:52 +0300

    feat: use Card Component in the Column

--- src/components/Column/index.tsx
+++ src/components/Column/index.tsx
@@ -1,11 +1,23 @@
 import { Column as ColumnType } from "@types";
+import Card from "components/Card";
 
 type ColumnProps = {
   id: ColumnType["id"];
 };
 
 function Column({ id }: ColumnProps): JSX.Element {
-  return <section>Column {id}</section>;
+  return (
+    <section>
+      <header>
+        <h3>Column {id}</h3>
+      </header>
+      <ol>
+        <li>
+          <Card id="card_32" />
+        </li>
+      </ol>
+    </section>
+  );
 }
 
 export default Column;
