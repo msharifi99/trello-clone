f760ec406206844ab664f2a2bd37e62c4773f81b
Mohammad Sharifi <mohammadsharifi9978@gmail.com>
2022-09-04 23:07:50 +0300

    feat: use useColumn in Column component

--- src/components/Column/index.tsx
+++ src/components/Column/index.tsx
@@ -1,20 +1,25 @@
 import { Column as ColumnType } from "@types";
 import Card from "components/Card";
+import useColumn from "hooks/useColumn";
 
 type ColumnProps = {
   id: ColumnType["id"];
 };
 
 function Column({ id }: ColumnProps): JSX.Element {
+  const { column } = useColumn(id);
+
   return (
     <section>
       <header>
-        <h3>Column {id}</h3>
+        <h3>{column.title}</h3>
       </header>
       <ol>
-        <li>
-          <Card id="card_32" />
-        </li>
+        {column.cardsId.map((id) => (
+          <li>
+            <Card id={id} />
+          </li>
+        ))}
       </ol>
     </section>
   );
