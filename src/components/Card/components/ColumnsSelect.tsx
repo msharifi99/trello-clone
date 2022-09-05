51dbd147f7df8bcfb0aaef4da4bd34d83ed6dafa
Mohammad Sharifi <mohammadsharifi9978@gmail.com>
2022-09-05 20:59:53 +0300

    feat: add Column select component
    this component is a select that its options are current board columns

---
+++ src/components/Card/components/ColumnsSelect.tsx
@@ -0,0 +1,30 @@
+import { Column } from "@types";
+import useBoard from "hooks/useBoard";
+import { ComponentProps } from "react";
+
+type ColumnSelectProps = {
+  excludeColumn: Column["id"];
+  onColumnChange: ComponentProps<"select">["onChange"];
+};
+
+function ColumnsSelect({
+  excludeColumn,
+  onColumnChange,
+}: ColumnSelectProps): JSX.Element {
+  const { columns } = useBoard();
+
+  return (
+    <select onChange={onColumnChange}>
+      <option selected disabled>
+        {columns.length > 1 ? "Select to move" : "No column available"}
+      </option>
+      {columns
+        .filter(({ id }) => id !== excludeColumn)
+        .map(({ title, id }) => (
+          <option value={id}>{title}</option>
+        ))}
+    </select>
+  );
+}
+
+export default ColumnsSelect;
