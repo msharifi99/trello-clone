86a1a7e221618be5ecb157779d0e5b683a5b5a3a
Mohammad Sharifi <mohammadsharifi9978@gmail.com>
2022-09-05 21:31:23 +0300

    feat: add input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+import { spacings } from "styles/variables";
+
+const Input = styled.input`
+  width: 100%;
+  padding: ${spacings.xxs};
+`;
+
+export default Input;
