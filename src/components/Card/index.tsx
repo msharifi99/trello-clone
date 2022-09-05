f58e0b14a2946df47f57b5985e500ced6d5f5710
Mohammad Sharifi <mohammadsharifi9978@gmail.com>
2022-09-05 20:50:42 +0300

    feat: use CardContainer component in Column

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,20 +1,49 @@
 import { Card as CardType } from "@types";
 import useCard from "hooks/useCard";
+import { useState } from "react";
+import CardContainer, { CardContainerProps } from "./components/Container";
 
 type CardProps = {
   id: CardType["id"];
 };
 
 function Card({ id }: CardProps): JSX.Element {
-  const { card } = useCard(id);
+  const { card, editCard, removeCard, isEditing, setIsEditing } = useCard(id);
+
+  const [cardTitle, setTitleInput] = useState(card.title);
+  const [cardDescription, setDescriptionInput] = useState(card.description);
+
+  const handleEditCard = () => {
+    editCard({ description: cardDescription, title: cardTitle });
+    setIsEditing(false);
+  };
+
+  const handleOnInputChange: CardContainerProps["onInputChange"] = (
+    inputName,
+    e
+  ) => {
+    if (inputName === "title") {
+      setTitleInput(e.target.value);
+      return;
+    }
+
+    if (inputName === "description") {
+      setDescriptionInput(e.target.value);
+      return;
+    }
+  };
 
   return (
-    <section>
-      <header>
-        <h2>{card.title}</h2>
-      </header>
-      <p>{card.description}</p>
-    </section>
+    <CardContainer
+      cardDescription={card.description}
+      cardTitle={card.title}
+      inputValues={{ title: cardTitle, description: cardDescription }}
+      isEditing={isEditing}
+      onEditButtonClick={() => setIsEditing(true)}
+      onInputChange={handleOnInputChange}
+      onRemove={removeCard}
+      onSave={handleEditCard}
+    ></CardContainer>
   );
 }
 
