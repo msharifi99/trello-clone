2634c8874ed2450219b0fab8f892a9c67fc7a737
Mohammad Sharifi <mohammadsharifi9978@gmail.com>
2022-09-05 19:35:37 +0300

    feat: add column and set it in edit mode

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,10 +1,20 @@
+import useBoard from "hooks/useBoard";
+import { ComponentProps } from "react";
 import BoardContainer from "./components/BoardContainer";
 import ColumnList from "./components/ColumnList";
 
 function Board(): JSX.Element {
+  const { addColumn, setEditingColumnId } = useBoard();
+
+  const handleAddColumn: ComponentProps<"button">["onClick"] = () => {
+    const column = addColumn({ cardsId: [], title: "New Column" });
+    setEditingColumnId(column.id);
+  };
+
   return (
     <BoardContainer>
       <ColumnList />
+      <button onClick={handleAddColumn}>Add Column</button>
     </BoardContainer>
   );
 }
