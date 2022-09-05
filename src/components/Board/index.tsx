8a447cb7f518dca58001baf580770bac850437c7
Mohammad Sharifi <mohammadsharifi9978@gmail.com>
2022-09-05 19:33:01 +0300

    refactor: use BoardContainer in Board component

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,40 +1,11 @@
-import styled from "@emotion/styled";
-import {
-  breakpoints,
-  headerHeight,
-  mainContainerPadding,
-  spacings,
-} from "styles/variables";
+import BoardContainer from "./components/BoardContainer";
 import ColumnList from "./components/ColumnList";
-
-const boardHeaderHeight = "2rem";
-const StyledColumnList = styled(ColumnList)`
-  height: calc(
-    100vh - ${headerHeight.sm} - ${boardHeaderHeight} -
-      ${mainContainerPadding.sm} * 2
-  );
-
-  @media (min-width: ${breakpoints.sm}) {
-    margin-left: ${spacings.md};
-  }
-`;
-
-const StyledTitle = styled.h2`
-  margin: 0;
-`;
-
-const StyledHeader = styled.header`
-  height: ${boardHeaderHeight};
-`;
 
 function Board(): JSX.Element {
   return (
-    <article>
-      <StyledHeader>
-        <StyledTitle>Test Board</StyledTitle>
-      </StyledHeader>
-      <StyledColumnList />
-    </article>
+    <BoardContainer>
+      <ColumnList />
+    </BoardContainer>
   );
 }
 
